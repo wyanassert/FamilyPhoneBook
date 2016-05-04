4364d5d3572ba71ecf05dd53ad1b01851e4a2c61
wy1402099772 <1402099772@qq.com>
2016-05-04 19:40:13 +0800

    add select UI

--- PhoneBook/app/src/main/java/com/example/administrator/phonebook/BlackListActivity.java
+++ PhoneBook/app/src/main/java/com/example/administrator/phonebook/BlackListActivity.java
@@ -1,17 +1,22 @@
 package com.example.administrator.phonebook;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
@@ -22,8 +27,12 @@ import java.util.ArrayList;
 
 public class BlackListActivity extends AppCompatActivity {
     private ListView listView;
+    private Button cancelButton;
+    private Button deleteButton;
+    private CheckBox allSelectCheckBox;
     private ArrayList<BlackListModel> modelArray;
     private MyAdapter listAdapter;
+    boolean isEditMode;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -43,6 +52,43 @@ public class BlackListActivity extends AppCompatActivity {
         listView = (ListView) this.findViewById(R.id.blacklist_listView);
         listAdapter = new MyAdapter(this);
         listView.setAdapter(listAdapter);
+        isEditMode = false;
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("Long Press", "did Click");
+                if(!isEditMode)
+                    transIntoEditMode();
+                return true;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("Click", "did Click");
+                ViewHolder0 holder = (ViewHolder0) view.getTag();
+                holder.checkBox.toggle();
+            }
+        });
+
+        cancelButton = (Button) this.findViewById(R.id.blacklist_cancel_button);
+        deleteButton = (Button) this.findViewById(R.id.blacklist_delete_button);
+        allSelectCheckBox = (CheckBox) this.findViewById(R.id.blacklist_allselect_check);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transOutOfEditMode();
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transOutOfEditMode();
+            }
+        });
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -74,6 +120,25 @@ public class BlackListActivity extends AppCompatActivity {
         super.onStop();
 
 
+    }
+
+    private void transIntoEditMode()
+    {
+        isEditMode = true;
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.blacklist_top_layout);
+        Resources resources = getApplicationContext().getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = 60 * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        layout.getLayoutParams().height = (int) px;
+        listAdapter.notifyDataSetChanged();
+    }
+
+    private void transOutOfEditMode()
+    {
+        isEditMode = false;
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.blacklist_top_layout);
+        layout.getLayoutParams().height = 0;
+        listAdapter.notifyDataSetChanged();
     }
 
     class MyAdapter extends BaseAdapter {
@@ -114,13 +179,13 @@ public class BlackListActivity extends AppCompatActivity {
 
             int type = getItemViewType(position);
             if (convertView == null) {
-                Log.e("convertView=", "null");
                 switch (type) {
                     case TYPE_0:
                         convertView = inflater.inflate(R.layout.active_black_list_cell, parent, false);
                         holder0 = new ViewHolder0();
                         holder0.phoneNumber = (TextView) convertView.findViewById(R.id.cell_blacklist_phonenumber);
                         holder0.note = (TextView) convertView.findViewById(R.id.cell_blacklist_note);
+                        holder0.checkBox = (CheckBox) convertView.findViewById(R.id.cell_blacklist_check);
                         convertView.setTag(holder0);
                         break;
 
