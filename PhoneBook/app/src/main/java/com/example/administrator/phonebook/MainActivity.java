09467b803bda16bc61fdf88bfee496328cd6cc6e
wy1402099772 <1402099772@qq.com>
2016-05-07 05:08:49 +0800

    complete main interface

--- PhoneBook/app/src/main/java/com/example/administrator/phonebook/MainActivity.java
+++ PhoneBook/app/src/main/java/com/example/administrator/phonebook/MainActivity.java
@@ -1,27 +1,47 @@
 package com.example.administrator.phonebook;
 
 import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private CallingFragment mCalling;
-    private PeopleFragment mPeople;
-
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    PhoneBookFragmentPageAdapter mFragmentPageAdapter;
+    ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentPageAdapter = new PhoneBookFragmentPageAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mFragmentPageAdapter);
+        mViewPager.setOnPageChangeListener(
+                new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        getSupportActionBar().setSelectedNavigationItem(position);
+                    }
+                });
 
         ActionBar bar = getSupportActionBar();
         bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -29,25 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ActionBar.TabListener tabListener = new ActionBar.TabListener() {
             public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
                 // show the given tab
-                FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
-                switch (tab.getPosition())
-                {
-                    case 0:
-                     if (mCalling == null)
-                     {
-                         mCalling = new CallingFragment();
-                     }
-                    transaction.replace(R.id.id_frame, mCalling);
-                        break;
-                    case 1:
-                    if (mPeople == null)
-                    {
-                        mPeople = new PeopleFragment();
-                    }
-                    transaction.replace(R.id.id_frame, mPeople);
-                        break;
-                }
-                transaction.commit();
+                mViewPager.setCurrentItem(tab.getPosition());
             }
 
             public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
@@ -62,7 +64,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bar.addTab(bar.newTab().setText("通话记录").setTabListener(tabListener));
         bar.addTab(bar.newTab().setText("联系人").setTabListener(tabListener));
 
-        setDefaultFragment();
+    }
+
+    public class PhoneBookFragmentPageAdapter extends FragmentPagerAdapter {
+        public PhoneBookFragmentPageAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new DemoObjectFragment();
+            Bundle args = new Bundle();
+            // Our object is just an integer :-P
+            args.putInt(DemoObjectFragment.ARG_OBJECT, i);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return Integer.toString(position);
+        }
+    }
+
+    public static class DemoObjectFragment extends Fragment {
+        public static final String ARG_OBJECT = "object";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // The last two arguments ensure LayoutParams are inflated
+            // properly.
+            Bundle args = getArguments();
+            int type = args.getInt(ARG_OBJECT);
+            View rootView = inflater.inflate(
+                    R.layout.fragment_calling, container, false);
+//            ((TextView) rootView.findViewById(R.id.text1)).setText(
+//                    Integer.toString(type));
+            if(0 == type)
+            {
+
+            }
+            else if(1 == type)
+            {
+
+            }
+            return rootView;
+        }
     }
 
     @Override
@@ -76,33 +128,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-//            case R.id.action_edit:
-//                Toast.makeText(this, "edit", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.action_add:
-//                Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.action_blacklist:
-//                jumpToBlackList();
-//                Toast.makeText(this, "blacklist", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.action_export:
-//                Toast.makeText(this, "export", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.action_about:
-//                Toast.makeText(this, "about", Toast.LENGTH_SHORT).show();
-//                return true;
+            case R.id.action_edit:
+                Toast.makeText(this, "edit", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_add:
+                Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_blacklist:
+                jumpToBlackList();
+                Toast.makeText(this, "blacklist", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_export:
+                Toast.makeText(this, "export", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_about:
+                Toast.makeText(this, "about", Toast.LENGTH_SHORT).show();
+                return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    private void setDefaultFragment() {
-        FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
-        mCalling = new CallingFragment();
-        transaction.replace(R.id.id_frame, mCalling);
-        transaction.commit();
     }
 
     @Override
